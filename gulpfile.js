4498d56654e6959b716c60e4f9de03cca591ce93
Cory Wilson <Cory@Corys-MacBook-Pro.local>
2015-07-08 19:58:16 -0400

    updated gulpfile and installed gulp-packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,29 @@
-var sass = require('gulp-sass');
+var gulp       = require('gulp'),
+    browserify = require('browserify'),
+    bower      = require('gulp-bower'),
+    minifyCss  = require('gulp-minify-css'),
+    nodemon    = require('gulp-nodemon'),
+    notify     = require('gulp-notify'),
+    rename     = require('gulp-rename'),
+    sass       = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    uglify     = require('gulp-uglify'),
+    watchify   = require('watchify');
 
 var config = {
-  bowerDir : './bower_components'
+  sassPath : './dev/sass',
+  jsPath   : './dev/js',
+	bowerDir : './bower_components'
 };
+
+gulp.task('sass', function(){
+  gulp.src(config.sassPath+'/main.scss')
+    .pipe(sourcemaps.init())
+      .pipe(sass())
+      .pipe(minifyCss())
+      .pipe(rename('main.min.css'))
+    .pipe(sourcemaps.write('../../../maps'))
+    .pipe(gulp.dest('./site/assets/css'));
+});
+
+gulp.task('default', ['sass']);
