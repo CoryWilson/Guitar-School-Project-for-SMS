4b92f9407005ddf65433a3aabdc8a643f77ef2ce
Cory Wilson <Cory@Corys-MacBook-Pro.local>
2015-07-27 18:39:04 -0400

    Added fretboard images to chord lessons.

--- dev/js/app.js
+++ dev/js/app.js
@@ -23,10 +23,6 @@ app.config(['$routeProvider','$locationProvider',function($routeProvider,$locati
       templateUrl : './partials/login.html',
       controller  : 'UserAuthController'
     })
-    .when('/profile',{
-      templateUrl : './partials/profile.html',
-      controller  : 'ProfileController'
-    })
     .when('/courses',{
       templateUrl : './partials/courses.html',
       controller  : 'CoursesController'
@@ -42,14 +38,6 @@ app.config(['$routeProvider','$locationProvider',function($routeProvider,$locati
 }]);
 
 /***** Factories *****/
-app.factory('UsersRef', ['FIREBASE_URI','$firebaseObject',
-  function(FIREBASE_URI,$firebaseObject) {
-    var ref = new Firebase(FIREBASE_URI);
-    var usersRef = ref.child('users');
-    return $firebaseObject(usersRef);
-  }
-]);
-
 app.factory('Auth', ['FIREBASE_URI','$firebaseAuth',
   function(FIREBASE_URI,$firebaseAuth) {
     var ref = new Firebase(FIREBASE_URI);
@@ -76,47 +64,20 @@ app.factory('Lessons', ['FIREBASE_URI','$firebaseAuth','$firebaseObject',
 
 /***** Controllers *****/
 
-app.controller('MainController', ['$scope','Auth',function($scope,Auth){
-  $scope.authObj = Auth;
-  $scope.courses = [
-    'Beginner',
-    'Intermediate',
-    'Expert'
-  ];
-  $scope.authObj.$onAuth(function(authData){
-    $scope.authData = authData;
+app.controller('MainController', ['$scope','$firebaseObject','Auth','Courses',function($scope,$firebaseObject,Auth,Courses){
+  var obj = Courses;
+
+  obj.$loaded().then(function() {
+    console.log("loaded record:", obj.$id, obj.someOtherKeyInData);
+
+    // To iterate the key/value pairs of the object, use angular.forEach()
+    angular.forEach(obj, function(value, key) {
+      console.log(key, value);
+    });
   });
+
+  $scope.data = obj;
 }]);
-
-app.factory('Profile', ['FIREBASE_URI','$firebaseObject',
-  function(FIREBASE_URI,$firebaseObject) {
-    return function(username) {
-      // create a reference to the database node where we will store our data
-      var randomRoomId = Math.round(Math.random() * 100000000);
-      var ref = new Firebase(UsersRef + randomRoomId);
-      var profileRef = ref.child(username);
-
-      // return it as a synchronized object
-      return $firebaseObject(profileRef);
-    };
-  }
-]);
-
-app.controller("ProfileController", ["$scope", "Profile",
-  function($scope, Profile) {
-    // put our profile in the scope for use in DOM
-    $scope.profile = Profile("physicsmarie");
-
-    // calling $save() on the synchronized object syncs all data back to our database
-    $scope.saveProfile = function() {
-      $scope.profile.$save().then(function() {
-        alert('Profile saved!');
-      }).catch(function(error) {
-        alert('Error!');
-      });
-    };
-  }
-]);
 
 app.controller('CoursesController', ['$scope','$firebaseObject','Auth','Courses',function($scope,$firebaseObject,Auth,Courses){
   var obj = Courses;
@@ -166,11 +127,9 @@ app.controller('LessonsController', ['$scope','$routeParams','$firebaseObject','
 }]);
 
 //User Authentication Controller
-app.controller('UserAuthController',['$scope','$firebaseObject','Auth','UsersRef',function($scope,$firebaseObject,Auth,UsersRef){
+app.controller('UserAuthController',['$scope','$firebaseObject','Auth',function($scope,$firebaseObject,Auth){
   $scope.authObj = Auth;
-
-  $scope.userObj = UsersRef;
-
+  
   $scope.registerUser = function(email,password){
     $scope.authObj.$createUser({
       email: $scope.email,
